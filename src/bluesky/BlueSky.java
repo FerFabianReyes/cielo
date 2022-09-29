21b3ad65f03423de6c53b7bef489a2f74e45fd9e
ferfabianreyes <2100721a@umich.mx>
2022-09-29 12:30:11 -0500

    Carga

--- src/bluesky/BlueSky.java
+++ src/bluesky/BlueSky.java
@@ -1,45 +1,22 @@
 
 package bluesky;
-import java.awt.Color;
-import java.awt.Dimension;
-import javax.swing.JPanel;
-import java.awt.Graphics;
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 
 
 public class BlueSky {
 
-
-    
     public static void main(String[] args) {
-       JFrame window = new JFrame("Swing");
-       MyCanvas canvas = new MyCanvas();
+        JFrame window = new JFrame("Swing");
+        MyCanvas canvas = new MyCanvas(); 
  
-       window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-       window.setSize(400, 300);
-       window.add(canvas);
-       window.pack();
-       window.setResizable(false);
-       window.setLocationRelativeTo(null);
-       window.setVisible(true);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(400, 300);
+        window.add(canvas);
+        window.pack();  // set the "size of frame to preferred sizes"
+        window.setResizable(false); // Do not change the size frame whit mouse
+        window.setLocationRelativeTo(null); // center of window
+        window.setVisible(true);
 
     }
-    
-    public class MyCanvas extends JPanel {
-    public MyCanvas () {
-        setPreferredSize(new Dimension(400,300));
-        setBackground(Color.GRAY);
-    }
-    @Override
-    protected void paintComponent(Graphics g){
-        super.paintComponent(g);
-        g.setColor(new Color (127,233,245));
-        g.fillRect(0,0, 400, 100);
-
-        g.setColor(Color.YELLOW);
-        g.fillOval(40, 40, 20, 20);
-    }
-}
 }
 
